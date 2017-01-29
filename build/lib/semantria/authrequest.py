d96e0c0d5d5caf194e301f456f2c023d09250628
Fabian Vergara <vergarf@gmail.com>
2017-01-29 01:33:26 -0500

    Semantria SDK

---
+++ build/lib/semantria/authrequest.py
@@ -0,0 +1,152 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from urllib.parse import urlparse, urlunparse, urlencode, quote
+except ImportError:
+    from urlparse import urlparse, urlunparse
+    from urllib import urlencode, quote
+
+import requests
+import json
+import time
+import random
+import hmac
+import binascii
+import hashlib
+import sys
+
+OAuthVersion = "1.0"
+OAuthParameterPrefix = "oauth_"
+OAuthConsumerKeyKey = "oauth_consumer_key"
+OAuthVersionKey = "oauth_version"
+OAuthSignatureMethodKey = "oauth_signature_method"
+OAuthSignatureKey = "oauth_signature"
+OAuthTimestampKey = "oauth_timestamp"
+OAuthNonceKey = "oauth_nonce"
+
+
+class AuthRequest:
+    apiVersion = "4.2"
+
+    def __init__(self, consumerKey, consumerSecret, applicationName, use_compression=False):
+        self.consumerKey = consumerKey
+        self.consumerSecret = consumerSecret
+        self.applicationName = applicationName
+        self.use_compression = use_compression
+
+    def authWebRequest(self, method, url, postData):
+        nonce = self.generateNonce()
+        timestamp = self.generateTimestamp()
+        query = self.generateQuery(method, url, timestamp, nonce)
+        authheader = self.generateAuthHeader(query, timestamp, nonce)
+
+        headers = {"Authorization": authheader}
+        if method == "POST":
+            headers["Content-type"] = "application/x-www-form-urlencoded"
+        headers["x-api-version"] = self.apiVersion
+        headers["x-app-name"] = self.applicationName
+        if self.use_compression:
+            headers["Accept-encoding"] = "gzip"
+
+        qparts = urlparse(query)
+        qscheme, qnetloc, qpath, qparams, qquery, qfragment = qparts[:6]
+
+        host = 'https://%s%s?%s' % (qnetloc, qpath, qquery)
+
+        params = {
+            'data': postData,
+            'headers': headers
+        }
+
+        response = requests.request(method, host, **params)
+
+        try:
+            data = json.dumps(response.json())
+        except ValueError:
+            data = ""
+
+        result = {"status": response.status_code, "reason": response.reason, "data": data}
+        return result
+
+    def getApiVersion(self):
+        return self.apiVersion
+
+    def setApiVersion(self, apiVersion):
+        self.apiVersion = apiVersion
+
+    def generateQuery(self, method, url, timestamp, nonce):
+        parts = urlparse(url)
+        scheme, netloc, path, params, query, fragment = parts[:6]
+        np = self.getNormalizedParameters(timestamp, nonce)
+
+        if query:
+            query = '%s&oauth_verifier=%s' % (query, np)
+        else:
+            query = '%s' % np
+        
+        return urlunparse((scheme, netloc, path, params, query, fragment))
+
+    def generateAuthHeader(self, query, timestamp, nonce):
+        md5cs = self.getMD5Hash(self.consumerSecret)
+        escquery = self.escape(query)
+        hash_ = self.getSHA1(md5cs, escquery)
+        hash_ = self.escape(hash_)
+
+        items = [
+            ("OAuth realm", ""),
+            (OAuthVersionKey, "%s" % OAuthVersion),
+            (OAuthTimestampKey, "%s" % timestamp),
+            (OAuthNonceKey, "%s" % nonce),
+            (OAuthSignatureMethodKey, "HMAC-SHA1"),
+            (OAuthConsumerKeyKey, "%s" % self.consumerKey),
+            (OAuthSignatureKey, "%s" % hash_)
+        ]
+        items.sort()
+
+        params = []
+        for k, v in items:
+            params.append('%s="%s"' % (k, v))
+
+        return ",".join(params)
+
+    def getNormalizedParameters(self, timestamp, nonce):
+        items = [
+            (OAuthVersionKey, OAuthVersion),
+            (OAuthTimestampKey, timestamp),
+            (OAuthNonceKey, nonce),
+            (OAuthSignatureMethodKey, "HMAC-SHA1"),
+            (OAuthConsumerKeyKey, self.consumerKey)
+        ]
+        items.sort()
+        
+        encoded_str = urlencode(items)
+        # Encode signature parameters per Oauth Core 1.0 protocol
+        # Spaces must be encoded with "%20" instead of "+"
+        return encoded_str.replace('+', '%20').replace('%7E', '~')
+
+    def getMD5Hash(self, str_):
+        md5h = hashlib.md5()
+        if sys.version_info[0] >= 3:
+            md5h.update(bytes(str_, 'utf-8'))
+        else:
+            md5h.update(str_)
+        md5sig = md5h.hexdigest()
+        return md5sig
+ 
+    def getSHA1(self, md5cs, query):
+        if sys.version_info[0] >= 3:  # Python 3
+            sha1res = hmac.new(bytes(md5cs, 'utf-8'), bytes(query, 'utf-8'), hashlib.sha1).digest()
+        else:
+            sha1res = hmac.new(md5cs, query, hashlib.sha1).digest()
+
+        sha1sig = binascii.b2a_base64(sha1res)[:-1]
+        return sha1sig
+
+    def generateTimestamp(self):
+        return int(time.time())
+
+    def generateNonce(self, length=20):
+        return ''.join([str(random.randint(0, 9)) for i in range(length)])
+    
+    def escape(self, s):
+        return quote(s, safe='~')
---
+++ tests/unittests.py
@@ -0,0 +1,273 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import unittest
+import uuid
+
+import semantria
+
+# the consumer key and secret
+consumerKey = ""
+consumerSecret = ""
+
+col_id = str(uuid.uuid1()).replace("-", "")
+doc_id = str(uuid.uuid1()).replace("-", "")
+message = "Amazon Web Services has announced a new feature called VMWare Import, " \
+          "which allows IT departments to move virtual machine images from their " \
+          "internal data centers to the cloud."
+
+
+def onRequest(sender, result):
+    # print("\n", "REQUEST: ", result)
+    pass
+
+
+def onResponse(sender, result):
+    # print("\n", "RESPONSE: ", result)
+    pass
+
+
+def onError(sender, result):
+    print("\n", "ERROR: ", result)
+
+
+def onDocsAutoResponse(sender, result):
+    # print("\n", "AUTORESPONSE: ", len(result), result)
+    pass
+
+
+def onCollsAutoResponse(sender, result):
+    # print("\n", "AUTORESPONSE: ", len(result), result)
+    pass
+
+
+class SemantriaSessionTest(unittest.TestCase):
+    def setUp(self):
+        # self.serializer = semantria.XmlSerializer()
+        self.serializer = semantria.JsonSerializer()
+        self.session = semantria.Session(consumerKey, consumerSecret, self.serializer, use_compression=True)
+
+        self.session.Request += onRequest
+        self.session.Response += onResponse
+        self.session.Error += onError
+        self.session.DocsAutoResponse += onDocsAutoResponse
+        self.session.CollsAutoResponse += onCollsAutoResponse
+
+    def test_CRUDBlacklist(self):
+        response = self.session.getBlacklist()
+        self.failIf(response is None)
+
+        response = self.session.addBlacklist({"name": "python3*"})
+        self.failIf(len(response) == 0)
+
+        blacklist = None
+        for item in response:
+            if item['name'] == 'python3*':
+                blacklist = item
+                break
+        self.failIf(blacklist is None)
+
+        status = self.session.removeBlacklist([blacklist['id']])
+        self.failIf(status != 202)
+
+    def test_CRUDCategories(self):
+        response = self.session.getCategories()
+        self.failIf(response is None)
+        response = self.session.addCategories([{
+            'name': "TEST_CATEGORY_PYTHON3",
+            'samples': ["Amazon", "VMWare"],
+            'weight': 0.1,
+        }])
+        self.failIf(len(response) == 0)
+
+        obj = None
+        for item in response:
+            if item['name'] == 'TEST_CATEGORY_PYTHON3':
+                obj = item
+                break
+        self.failIf(obj is None)
+
+        response = self.session.removeCategories([obj['id']])
+        self.failIf(response != 202)
+
+    def test_CRUDQueries(self):
+        response = self.session.getQueries()
+        self.failIf(response is None)
+
+        response = self.session.addQueries([{"name": "TEST_QUERY_PYTHON3", "query": "Amazon"}])
+        self.failIf(len(response) == 0)
+
+        obj = None
+        for item in response:
+            if item['name'] == 'TEST_QUERY_PYTHON3':
+                obj = item
+                break
+        self.failIf(obj is None)
+
+        response = self.session.removeQueries([obj['id']])
+        self.failIf(response != 202)
+
+    def test_CRUDPhrases(self):
+        response = self.session.getPhrases()
+        self.failIf(response is None)
+
+        response = self.session.addPhrases([
+            {"name": "TEST_PHRASE_PYTHON3", "weight": 0.1},
+        ])
+        self.failIf(len(response) == 0)
+
+        obj = None
+        for item in response:
+            if item['name'] == 'TEST_PHRASE_PYTHON3':
+                obj = item
+                break
+        self.failIf(obj is None)
+
+        response = self.session.removePhrases([obj['id']])
+        self.failIf(response != 202)
+
+    def test_CRUDEntities(self):
+        response = self.session.getEntities()
+        self.failIf(response is None)
+
+        response = self.session.addEntities([
+            {"name": "TEST_ENTITY_PYTHON3", "type": "Web"},
+        ])
+        self.failIf(len(response) == 0)
+
+        obj = None
+        for item in response:
+            if item['name'] == 'TEST_ENTITY_PYTHON3':
+                obj = item
+                break
+        self.failIf(obj is None)
+
+        response = self.session.removeEntities([obj['id']])
+        self.failIf(response != 202)
+
+    def test_CRUDTaxonomy(self):
+        response = self.session.getTaxonomy()
+        self.failIf(response is None)
+
+        response = self.session.addTaxonomy([
+            {"name": "TEST_TAXONOMY_PYTHON3"},
+        ])
+        self.failIf(len(response) == 0)
+
+        obj = None
+        for item in response:
+            if item['name'] == 'TEST_TAXONOMY_PYTHON3':
+                obj = item
+                break
+        self.failIf(obj is None)
+
+        response = self.session.removeTaxonomy([obj['id']])
+        self.failIf(response != 202)
+
+    def test_CreateDocument(self):
+        status = self.session.queueDocument({"id": doc_id, "text": message})
+        self.failIf(status != 200 and status != 202)
+
+    def test_GetDocument(self):
+        status = self.session.getDocument(doc_id)
+        self.failIf(status is None)
+
+    def test_CreateBatchDocuments(self):
+        batch = []
+        var = 10
+        while var > 0:
+            batch.append({"id": str(uuid.uuid1()).replace("-", ""), "text": message})
+            var -= 1
+        status = self.session.queueBatch(batch)
+        self.failIf(status != 200 and status != 202)
+
+    def test_GetProcessedDocuments(self):
+        status = self.session.getProcessedDocuments()
+        self.failIf(status is None)
+
+    def test_CreateCollection(self):
+        status = self.session.queueCollection({"id": col_id, "documents": [message, message]})
+        self.failIf(status != 200 and status != 202)
+
+    def test_GetCollection(self):
+        status = self.session.getCollection(col_id)
+        self.failIf(status is None)
+
+    def test_GetProcessedCollections(self):
+        status = self.session.getProcessedCollections()
+        self.failIf(status is None)
+
+    def test_Status(self):
+        status = self.session.getStatus()
+        self.failIf(status is None)
+
+    def test_VerifySubscription(self):
+        status = self.session.getSubscription()
+        self.failIf(status is None)
+
+    def test_CRUDConfigurations(self):
+        status = self.session.getConfigurations()
+        self.failIf(status is None)
+
+        test_configuration = {
+            'auto_response': False,
+            'is_primary': False,
+            'name': 'TEST_CONFIG_PYTHON3',
+            'language': 'English',
+            'document': {
+                'query_topics_limit': 5,
+                'concept_topics_limit': 5,
+                'named_entities_limit': 5,
+                'user_entities_limit': 5
+            }
+        }
+
+        response = self.session.addConfigurations([test_configuration])
+        self.failIf(len(response) == 0)
+
+        obj = None
+        for item in response:
+            if item["name"] == "TEST_CONFIG_PYTHON3":
+                obj = item
+                break
+        self.failIf(obj is None)
+        self.failIf(obj['chars_threshold'] != 80)
+
+        obj['chars_threshold'] = 20
+        config_id = obj['id']
+
+        response = self.session.updateConfigurations([obj])
+        self.failIf(len(response) == 0)
+        obj = None
+        for item in response:
+            if item["id"] == config_id:
+                obj = item
+                break
+        self.failIf(obj is None)
+        self.failIf(obj['chars_threshold'] != 20)
+
+        response = self.session.removeConfigurations([obj['id']])
+        self.failIf(response != 202)
+
+if __name__ == '__main__':
+    # unittest.main()
+
+    suite = unittest.TestSuite()
+    suite.addTest(SemantriaSessionTest('test_Status'))
+    suite.addTest(SemantriaSessionTest('test_VerifySubscription'))
+    suite.addTest(SemantriaSessionTest('test_CRUDConfigurations'))
+    suite.addTest(SemantriaSessionTest('test_CRUDBlacklist'))
+    suite.addTest(SemantriaSessionTest('test_CRUDCategories'))
+    suite.addTest(SemantriaSessionTest('test_CRUDQueries'))
+    suite.addTest(SemantriaSessionTest('test_CRUDPhrases'))
+    suite.addTest(SemantriaSessionTest('test_CRUDEntities'))
+
+    suite.addTest(SemantriaSessionTest('test_CreateDocument'))
+    suite.addTest(SemantriaSessionTest('test_GetDocument'))
+    suite.addTest(SemantriaSessionTest('test_CreateBatchDocuments'))
+    suite.addTest(SemantriaSessionTest('test_GetProcessedDocuments'))
+
+    suite.addTest(SemantriaSessionTest('test_CreateCollection'))
+    suite.addTest(SemantriaSessionTest('test_GetCollection'))
+    suite.addTest(SemantriaSessionTest('test_GetProcessedCollections'))
+
+    unittest.TextTestRunner(verbosity=2).run(suite)
